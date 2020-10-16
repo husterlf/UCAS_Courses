00a059053b19ae3b80ca03faa66d5cc533639664
陆峰 <lufeng.ict@gmail.com>
2020-10-16 20:54:37 +0800

    to solve test01.c

--- Advanced Compiling Techniques/ast-interpreter/ASTInterpreter.cpp
+++ Advanced Compiling Techniques/ast-interpreter/ASTInterpreter.cpp
@@ -12,68 +12,144 @@ using namespace clang;
 
 #include "Environment.h"
 
-class InterpreterVisitor : 
-   public EvaluatedExprVisitor<InterpreterVisitor> {
+class InterpreterVisitor : public EvaluatedExprVisitor<InterpreterVisitor>
+{
 public:
-   explicit InterpreterVisitor(const ASTContext &context, Environment * env)
-   : EvaluatedExprVisitor(context), mEnv(env) {}
+   explicit InterpreterVisitor(const ASTContext &context, Environment *env)
+       : EvaluatedExprVisitor(context), mEnv(env) {} //this class provide visit interfaces
    virtual ~InterpreterVisitor() {}
 
-   virtual void VisitBinaryOperator (BinaryOperator * bop) {
-	   VisitStmt(bop);
-	   mEnv->binop(bop);
+   virtual void VisitBinaryOperator(BinaryOperator *bop)
+   {
+      VisitStmt(bop);
+      mEnv->binop(bop);
    }
-   virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->declref(expr);
+   virtual void VisitDeclRefExpr(DeclRefExpr *expr)
+   {
+      VisitStmt(expr);
+      mEnv->declref(expr);
    }
-   virtual void VisitCastExpr(CastExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->cast(expr);
+   virtual void VisitCastExpr(CastExpr *expr)
+   {
+      VisitStmt(expr);
+      mEnv->cast(expr);
    }
-   virtual void VisitCallExpr(CallExpr * call) {
-	   VisitStmt(call);
-	   mEnv->call(call);
+   virtual void VisitCallExpr(CallExpr *call)
+   {
+      VisitStmt(call);
+      if (mEnv->isBuiltInFunc(call))
+         mEnv->callBuiltIn(call);
+      else{
+         if(FunctionDecl *fdecl=call->getDirectCallee()){
+            //custom func with a new stack
+            mEnv->callCustom(call);
+            Visit(fdecl->getBody());
+            if(!fdecl->isNoReturn()){
+               int64_t retvalue = mEnv->getCallReturn();
+               mEnv->callCustomFinished();
+               mEnv->pushStmVal(call,retvalue);
+            }
+            else
+               mEnv->callCustomFinished();
+
+         }
+
+         
+         
+      }
+         
    }
-   virtual void VisitDeclStmt(DeclStmt * declstmt) {
-	   mEnv->decl(declstmt);
+   virtual void VisitDeclStmt(DeclStmt *declstmt)
+   {
+      mEnv->decl(declstmt);
    }
+   virtual void VisitReturnStmt(ReturnStmt *returnStmt)
+   {
+      Visit(returnStmt->getRetValue());
+      mEnv->returnstmt(returnStmt);
+   }
+   virtual void VisitIfStmt(IfStmt *ifstmt)
+   {
+      Expr *cond = ifstmt->getCond();
+      if (mEnv->expr(cond))
+      { // True
+         Stmt *thenstmt = ifstmt->getThen();
+         Visit(thenstmt); //clang/AST/EvaluatedExprVisitor.h line 100
+      }
+      else
+      {
+         if (ifstmt->getElse())
+         {
+            Stmt *elsestmt = ifstmt->getElse();
+            Visit(elsestmt);
+         }
+      }
+   }
+
 private:
-   Environment * mEnv;
+   Environment *mEnv;
 };
 
-class InterpreterConsumer : public ASTConsumer {
+class InterpreterConsumer : public ASTConsumer
+{
 public:
-   explicit InterpreterConsumer(const ASTContext& context) : mEnv(),
-   	   mVisitor(context, &mEnv) {
+   explicit InterpreterConsumer(const ASTContext &context) : mEnv(),
+                                                             mVisitor(context, &mEnv)
+   {
    }
    virtual ~InterpreterConsumer() {}
 
-   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-	   TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
-	   mEnv.init(decl);
+   virtual void HandleTranslationUnit(clang::ASTContext &Context)
+   {
+      TranslationUnitDecl *decl = Context.getTranslationUnitDecl();
+      mEnv.init(decl);
       
-	   FunctionDecl * entry = mEnv.getEntry();
-      
-	   mVisitor.VisitStmt(entry->getBody());
-  }
+      FunctionDecl *entry = mEnv.getEntry();
+      mVisitor.VisitStmt(entry->getBody());
+   }
+
 private:
    Environment mEnv;
    InterpreterVisitor mVisitor;
 };
 
-class InterpreterClassAction : public ASTFrontendAction {
-public: 
-  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new InterpreterConsumer(Compiler.getASTContext()));
-  }
+class InterpreterClassAction : public ASTFrontendAction
+{
+public:
+   virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+       clang::CompilerInstance &Compiler, llvm::StringRef InFile)
+   {
+      return std::unique_ptr<clang::ASTConsumer>(
+          new InterpreterConsumer(Compiler.getASTContext()));
+   }
 };
 
+std::string exec(const char *cmd)
+{
+   FILE *pipe = popen(cmd, "r");
+   if (!pipe)
+      return "ERROR";
+   char buffer[128];
+   std::string result = "";
+   while (!feof(pipe))
+   {
+      if (fgets(buffer, 128, pipe) != NULL)
+         result += buffer;
+   }
+   pclose(pipe);
+   return result;
+}
 
-int main (int argc, char ** argv) {
-   if (argc > 1) {
-       clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
+int main(int argc, char **argv)
+{
+   if (argc > 1)
+   {
+      std::cout << "input param: " << argv[1] << std::endl;
+
+      std::string s = exec(argv[1]);
+      std::cout << "code content: \n\n"
+                << s << std::endl;
+
+      clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), s);
    }
 }
--- Advanced Compiling Techniques/ast-interpreter/Environment.h
+++ Advanced Compiling Techniques/ast-interpreter/Environment.h
@@ -1,6 +1,7 @@
 //==--- tools/clang-check/ClangInterpreter.cpp - Clang Interpreter tool --------------===//
 //===----------------------------------------------------------------------===//
 #include <stdio.h>
+#include <vector>
 
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/Decl.h"
@@ -8,6 +9,8 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
+
+#define DECL_DEFAULT_VALUE 0
 
 using namespace clang;
 
@@ -19,6 +22,9 @@ class StackFrame
 	std::map<Stmt *, int> mExprs;
 	/// The current stmt
 	Stmt *mPC;
+
+	int mReturnType=-1;
+	int64_t mReturnValue=0;
 
 public:
 	StackFrame() : mVars(), mExprs(), mPC()
@@ -50,6 +56,14 @@ public:
 	Stmt *getPC()
 	{
 		return mPC;
+	}
+	void setReturn(int64_t val){
+		//mReturnType=type;
+		mReturnValue=val;
+	}
+
+	int64_t getReturnValue(){
+		return mReturnValue;
 	}
 };
 
@@ -84,6 +98,7 @@ public:
 	/// Initialize the Environment
 	void init(TranslationUnitDecl *unit)
 	{
+		mStack.push_back(StackFrame()); //first should create a stack
 		for (TranslationUnitDecl::decl_iterator i = unit->decls_begin(), e = unit->decls_end(); i != e; ++i)
 		{
 			if (FunctionDecl *fdecl = dyn_cast<FunctionDecl>(*i))
@@ -98,9 +113,29 @@ public:
 					mOutput = fdecl;
 				else if (fdecl->getName().equals("main"))
 					mEntry = fdecl;
+				else
+				{	//other decl func
+					//mStack.back().bindStmt();
+				}
+			}
+			else if (VarDecl *vdecl = dyn_cast<VarDecl>(*i))
+			{ //global var
+				if (vdecl->hasInit())
+				{
+					if (auto intLiteral = dyn_cast<IntegerLiteral>(vdecl->getInit()))
+						mStack.back().bindDecl(vdecl, intLiteral->getValue().getSExtValue());
+				}
+				else
+				{
+					mStack.back().bindDecl(vdecl, DECL_DEFAULT_VALUE);
+				}
+				//int val = intLiteral->getValue().getSExtValue();
+				//mStack.back().bindStmt(left, val);
+			}
+			else
+			{ //other decl type
 			}
 		}
-		mStack.push_back(StackFrame());
 	}
 
 	FunctionDecl *getEntry()
@@ -120,32 +155,52 @@ public:
 			if (DeclRefExpr *declexpr = dyn_cast<DeclRefExpr>(left))
 			{
 				if (auto intLiteral = dyn_cast<IntegerLiteral>(right))
-				{//left is Interger Type
-					int val=intLiteral->getValue().getSExtValue();
-					mStack.back().bindStmt(left,val);
+				{ //right is Interger Type
+					int val = intLiteral->getValue().getSExtValue();
+					mStack.back().bindStmt(left, val);
 					Decl *decl = declexpr->getFoundDecl();
 					mStack.back().bindDecl(decl, val);
-					
 				}
-				else if(auto callType=dyn_cast<CallExpr>(right))
+				else if (auto callType = dyn_cast<CallExpr>(right))
 				{
 					int val = mStack.back().getStmtVal(right);
 					mStack.back().bindStmt(left, val);
 					Decl *decl = declexpr->getFoundDecl();
 					mStack.back().bindDecl(decl, val);
 				}
-				else{
+				else
+				{
 					//bind 0 if not include
-					mStack.back().bindStmt(left,0);
+					mStack.back().bindStmt(left, 0);
 					Decl *decl = declexpr->getFoundDecl();
-				    mStack.back().bindDecl(decl, 0);
+					mStack.back().bindDecl(decl, 0);
 				}
-
-				
 			}
 			else
 			{
 			}
+		}
+		else
+		{
+			int64_t result;
+			switch (bop->getOpcode())
+			{
+			case BO_GT:
+				result = expr(left) > expr(right);
+				break;
+			case BO_Add:
+				result = expr(left) + expr(right);
+				break;
+			case BO_Sub:
+				result = expr(left) - expr(right);
+				break;
+
+			default:
+				//undefined bop
+				break;
+			}
+
+			mStack.back().bindStmt(bop, result);
 		}
 	}
 
@@ -157,11 +212,15 @@ public:
 			Decl *decl = *it;
 			if (VarDecl *vardecl = dyn_cast<VarDecl>(decl))
 			{
-				if (vardecl->hasInit()) //add init handle
-					mStack.back().bindDecl(vardecl, 100);
+				if (vardecl->hasInit()) //add init handler,only for int
+				{
+					if (auto intLiteral = dyn_cast<IntegerLiteral>(vardecl->getInit()))
+						mStack.back().bindDecl(vardecl, intLiteral->getValue().getSExtValue());
+				}
 				else
 				{
-					mStack.back().bindDecl(vardecl, 0);
+					//default value
+					mStack.back().bindDecl(vardecl, DECL_DEFAULT_VALUE);
 				}
 			}
 		}
@@ -189,9 +248,19 @@ public:
 		}
 	}
 
-	/// !TODO Support Function Call
-	void call(CallExpr *callexpr)
+	bool isBuiltInFunc(CallExpr *callexpr)
 	{
+		FunctionDecl *callee = callexpr->getDirectCallee();
+		if (callee == mInput || callee == mOutput || callee == mMalloc || callee == mFree)
+			return true;
+
+		return false;
+	}
+
+	/// !TODO Support Function Call
+	void callBuiltIn(CallExpr *callexpr)
+	{
+		std::cout << "callBuiltIn" << std::endl;
 		mStack.back().setPC(callexpr);
 		int val = 0;
 		FunctionDecl *callee = callexpr->getDirectCallee();
@@ -204,13 +273,80 @@ public:
 		}
 		else if (callee == mOutput)
 		{
-			Expr *decl = callexpr->getArg(0);
+			Expr *decl = callexpr->getArg(0); //获取第一个参数
 			val = mStack.back().getStmtVal(decl);
 			llvm::errs() << val;
 		}
+		else if (callee == mMalloc)
+		{
+		}
+		else if (callee == mFree)
+		{
+		}
+	}
+	void callCustom(CallExpr *callexpr)
+	{
+		std::vector<int64_t> args;
+
+		for (auto i = callexpr->arg_begin(), e = callexpr->arg_end(); i != e; i++)
+		{
+			args.push_back(expr(*i));
+		}
+		mStack.push_back(StackFrame());
+		int j = 0;
+		FunctionDecl *callee = callexpr->getDirectCallee();
+		for (auto i = callee->param_begin(), e = callee->param_end(); i != e; i++, j++)
+		{
+			mStack.back().bindDecl(*i, args[j]);
+		}
+	}
+	void callCustomFinished()
+	{
+		if (!mStack.empty())
+			mStack.pop_back();
+	}
+	int64_t getCallReturn()
+	{
+		return mStack.back().getReturnValue();
+	}
+
+	void pushStmVal(Stmt *stmt, int64_t retvalue)
+	{
+		mStack.back().bindStmt(stmt, retvalue);
+	}
+	void returnstmt(ReturnStmt *stmt)
+	{
+		int64_t value = expr(stmt->getRetValue());
+		
+		mStack.back().setReturn(value);
+	}
+
+	int64_t expr(Expr *exp)
+	{
+		exp = exp->IgnoreImpCasts();
+		if (auto intLiteral = dyn_cast<IntegerLiteral>(exp))
+		{
+			llvm::APInt result = intLiteral->getValue();
+			return result.getSExtValue();
+		}
+		else if (auto callExpr = dyn_cast<CallExpr>(exp))
+		{
+			return mStack.back().getStmtVal(callExpr);
+		}
+		else if (auto declRef = dyn_cast<DeclRefExpr>(exp))
+		{
+			declref(declRef);
+			int64_t result = mStack.back().getStmtVal(declRef);
+			return result;
+		}
+		else if (auto binaryExpr = dyn_cast<BinaryOperator>(exp))
+		{ //+ - * / < > ==
+			binop(binaryExpr);
+			int64_t result = mStack.back().getStmtVal(binaryExpr);
+			return result;
+		}
 		else
 		{
-			/// You could add your code here for Function call Return
 		}
 	}
 };
